eb7b66d477f4cd0f1f8ee293496a8bf5ae8d219f
Ritika Gupta <131886644+Ritika277@users.noreply.github.com>
2024-09-08 19:35:38 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const nav = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const navBtnImg = document.querySelector("#nav-btn-img");
+
+//Preloader
+function hidePreloader() {
+  const preloader = document.getElementById("preloader");
+  preloader.style.display = "none";
+}
+
+window.addEventListener("load", function () {
+  setTimeout(hidePreloader, 1700);
+});
+
+//Hamburger menu
+navBtn.onclick = () => {
+  if (nav.classList.toggle("open")) {
+    navBtnImg.src = "img/icons/close.svg";
+  } else {
+    navBtnImg.src = "img/icons/open.svg";
+  }
+};
+
+//Sticky header & goToTop button
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("#header");
+  const hero = document.querySelector("#home");
+  let triggerHeight = hero.offsetHeight - 170;
+
+  if (window.scrollY > triggerHeight) {
+    header.classList.add("header-sticky");
+    goToTop.classList.add("reveal");
+  } else {
+    header.classList.remove("header-sticky");
+    goToTop.classList.remove("reveal");
+  }
+});
+
+//AOS animations settings
+AOS.init({
+  once: true,
+});
